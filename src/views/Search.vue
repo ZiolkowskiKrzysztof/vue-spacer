262d9bdba397dfa353f91be45a6f0335aaf7e000
krzysztof <ziolkowski15@gmail.com>
2019-11-23 16:24:11 +0100

    Create api connection

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -8,22 +8,39 @@
         v-model="searchValue"
         @input="handleInput"
       />
+      <ul>
+        <li v-for="item in results" :key="item.data[0].nasa_id">
+          <p>{{ item.data[0].description }}</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+
+const API = 'https://images-api.nasa.gov/search';
+
 export default {
   name: 'Search',
   data() {
     return {
       searchValue: '',
+      results: [],
     };
   },
   methods: {
-    handleInput() {
-      console.log(this.searchValue);
-    },
+    handleInput: debounce(function () {
+      axios.get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then((response) => {
+          this.results = response.data.collection.items;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 500),
   },
 };
 </script>
